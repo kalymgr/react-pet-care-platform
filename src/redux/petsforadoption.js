7410bfcba293eb99023b7e0c39abe5ff819670a5
Michalis <mtsougranis@gmail.com>
2020-02-22 20:56:03 +0200

    User story UID 6 task T1 completed.

--- src/redux/petsforadoption.js
+++ src/redux/petsforadoption.js
@@ -13,6 +13,17 @@ export const PetsForAdoption = (
         case ActionTypes.ADD_PETFORADOPTION:
             var petForAdoption = action.payload;
             return {...state, isLoading: false, errMess: null, petsForAdoption: state.petsForAdoption.concat(petForAdoption)}
+        case ActionTypes.ADD_PETSFORADOPTIONINFO:
+            return {
+                ...state, isLoading: false, errMess: null,
+                petsForAdoption: action.payload.petsForAdoptionInfo, 
+                pageNumber: action.payload.pageNumber, 
+                lastPageNumber: parseInt(action.payload.lastPageNumber)
+            }
+        case ActionTypes.PETSFORADOPTIONINFO_LOADING:
+            return {...state, isLoading: true, errMess: null, petsForAdoption: []};
+        case ActionTypes.PETSFORADOPTIONINFO_FAILED:
+            return {...state, isLoading: false, errMess: action.payload};
         default: 
             return state;
     }
